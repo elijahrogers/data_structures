b425158bd833a98b00f1b745aa1cd55bf8482abe
Elijah Rogers <elijahrogers@charter.net>
2022-03-18 07:23:58 -0700

    Add insertion sort

---
+++ test/sortable_array.test.js
@@ -0,0 +1,42 @@
+const SortableArray = require('../lib/sortable_array')
+
+test('Successfully intitializes', () => {
+  const sortable = new SortableArray()
+  expect(sortable).not.toBeUndefined()
+})
+
+describe('#insertionSort', () => {
+  describe('when no array is given', () => {
+    test('returns an empty array', () => {
+      const sortable = new SortableArray()
+
+      expect(sortable.insertionSort()).toEqual([])
+    })
+  })
+
+  describe('when array is given', () => {
+    describe('containing multiple data types', () => {
+      test('returns sorted array', () => {
+        const sortable = new SortableArray([3, 'x', 1, 5.3, 4])
+
+        expect(sortable.insertionSort()).toEqual([1, 3, 4, 5.3, 'x'])
+      })
+    })
+
+    describe('containing duplicate values', () => {
+      test('returns sorted array', () => {
+        const sortable = new SortableArray([3, 3, 2, 1, 4, 5])
+
+        expect(sortable.insertionSort()).toEqual([1, 2, 3, 3, 4, 5])
+      })
+    })
+
+    describe('containing null values', () => {
+      test('returns sorted array with nulls first', () => {
+        const sortable = new SortableArray([null, 3, null, 1, 4, 5])
+
+        expect(sortable.insertionSort()).toEqual([null, null, 1, 3, 4, 5])
+      })
+    })
+  })
+})
